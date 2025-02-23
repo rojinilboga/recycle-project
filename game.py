908621eb81f9fddec6a771bddf3a6ad910d62373
rojinilboga <rojin@ilboga.nl>
2025-02-23 11:55:37 +0100

    Add files via upload

--- game.py
+++ game.py
@@ -25,6 +25,37 @@ def update():
     pass
 
 
+def get_option(extra_items):
+    items_to_create = ["paper"]
+    for i in range(extra_items):
+        random_option = random.choice(ITEMS)
+        items_to_create.append(random_option)
+    return items_to_create
+
+def create_items(items_to_create):
+    new_items = []
+    for option in items_to_create:
+        item = Actor(option +'img')
+        new_items.append(item)
+    return new_items
+
+def layout_items(items_to_layout):
+    number_of_gaps = len(items_to_layout) +1
+    gap_size = WIDTH/number_of_gaps
+    random.shuffle(items_to_layout)
+    for index,item in enumerate(items_to_layout):
+        new_x_pos = (index +1) *gap_size
+        item.x = new_x_pos
+
+
+
+
+
+        
+                    
+
+
+
 
 
 
