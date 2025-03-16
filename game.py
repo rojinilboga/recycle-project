5d9894fe72b4cb1e171d9df5db18100233000f17
rojinilboga <rojin@ilboga.nl>
2025-03-16 11:53:25 +0100

    Add files via upload

--- game.py
+++ game.py
@@ -47,6 +47,51 @@ def layout_items(items_to_layout):
         new_x_pos = (index +1) *gap_size
         item.x = new_x_pos
 
+def animate_items(items_to_animate):
+    global animations
+    for item in items_to_animate:
+        duration = START_SPEED - current_level
+        item.anchor = ("center", "bottom")
+        animation = animate(item, duration=duration, on_finished=handle_game_over(),y=HEIGHT)
+        animations.append(animation)
+
+def handle_game_over():
+    global game_over
+    game_over = True
+
+def on_mouse_down(pos):
+    global items, current_level
+    for item in items:
+        if item.collidepoint(pos):
+            if "paper" in item.image:
+                handle_game_complete()
+            else:
+                handle_game_over()
+
+def handle_game_complete():
+    global current_level, items, animations, game_complete
+    stop_animations(animations)
+    if current_level == FINAL_LEVEL:
+        game_complete = True
+    else:
+        current_level += 1
+        items = []
+        animations = []
+
+def stop_animations(animations_to_stop):
+    for animation in animations_to_stop:
+        if animation.running:
+            animation.stop()
+
+def display_msg(heading_text, sub_text):
+    screen.draw.text(heading_text, fontsize=60, center=CENTER, color="black")
+    screen.draw.text(sub_text, fontsize=30, center=(CENTER_X,CENTER_Y +30 ),color="black")
+    
+
+                
+
+
+
 
 
 
